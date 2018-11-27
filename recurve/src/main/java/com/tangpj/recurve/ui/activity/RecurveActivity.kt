9ed9bf6a18a1767f300a5a4e8540904642c2c2a0
TPJ <514820184@qq.com>
2018-11-27 23:40:12 +0800

    add ui

---
+++ recurve/src/main/java/com/tangpj/recurve/ui/activity/RecurveActivity.kt
@@ -0,0 +1,12 @@
+package com.tangpj.recurve.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecurveActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ recurve/src/main/java/com/tangpj/recurve/ui/fragment/RecurveFragment.kt
@@ -0,0 +1,2 @@
+package com.tangpj.recurve.ui.fragment
+
