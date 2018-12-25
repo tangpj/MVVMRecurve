b454215fa2e4846e5d3192cf86e5c2ed5e4ae1ca
TPJ <514820184@qq.com>
2018-12-25 17:10:43 +0800

    add collasing_test

--- app/src/main/java/com/tangpj/mvvmrecurve/MainActivity.kt
+++ app/src/main/java/com/tangpj/mvvmrecurve/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.tangpj.mvvmrecurve
 
 import android.os.Bundle
+import com.tangpj.mvvmrecurve.databinding.CollasingTestBinding
 import com.tangpj.mvvmrecurve.databinding.ContentTestBinding
 import com.tangpj.recurve.ui.acitivty.RecurveActivity
-import com.tangpj.recurve.ui.appbar
 
 class MainActivity: RecurveActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = initContentBinding<ContentTestBinding>(R.layout.content_test)
-        appbar { toolBar { title =  "测试测试" }}
+        appbar {
+            collapsingToolbar {
+                toolBar {
+                    title = "测试测试"
+                }
+                collapsingContent { inflater, collapsingToolbarLayout ->
+                    val content = CollasingTestBinding.inflate(inflater, collapsingToolbarLayout, false)
+                    content.root
+                }
+            }
+        }
     }
 }
