038d3026053750d6587d2e09d816471b78801b69
TPJ <514820184@qq.com>
2019-01-06 22:58:21 +0800

    完善DaggerRecurveActivity

--- recurve-dagger2-support/src/main/java/com/tangpj/recurve/dagger2/DaggerRecurveActivity.kt
+++ recurve-dagger2-support/src/main/java/com/tangpj/recurve/dagger2/DaggerRecurveActivity.kt
@@ -9,25 +9,34 @@ import androidx.databinding.ViewDataBinding
 import com.tangpj.recurve.R
 import com.tangpj.recurve.databinding.ActivityRecurveBinding
 import com.tangpj.recurve.databinding.ToolbarRecurveBinding
+import com.tangpj.recurve.ui.appbar
 import com.tangpj.recurve.ui.creator.AppbarCreator
+import com.tangpj.recurve.ui.creator.ContentCreate
+import com.tangpj.recurve.ui.creator.RecurveAppbarCreator
+import com.tangpj.recurve.ui.creator.RecurveContentCreate
+import com.tangpj.recurve.ui.creator.ext.AppbarExt
 import dagger.android.support.DaggerAppCompatActivity
 
-abstract class DaggerRecurveActivity: DaggerAppCompatActivity() {
+abstract class DaggerRecurveActivity: DaggerAppCompatActivity(), ContentCreate {
 
     private lateinit var activityRecurveBinding: ActivityRecurveBinding
+
+    private lateinit var appbarCreator: AppbarCreator
+    private lateinit var contentCreate: ContentCreate
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityRecurveBinding = DataBindingUtil
                 .setContentView(this, R.layout.activity_recurve)
-
+        contentCreate = RecurveContentCreate(activityRecurveBinding)
+        appbarCreator = RecurveAppbarCreator(activityRecurveBinding)
     }
 
-    fun <T: ViewDataBinding> setRecurveContent(@LayoutRes layoutId: Int): T
-            = DataBindingUtil.setContentView(this, layoutId)
+    override fun <Binding : ViewDataBinding> initContentBinding(@LayoutRes layoutId: Int): Binding
+            = contentCreate.initContentBinding(layoutId)
 
-
-
-
+    fun appbar(init: AppbarExt.() -> Unit){
+        appbar(appbarCreator, init)
+    }
 
 }
