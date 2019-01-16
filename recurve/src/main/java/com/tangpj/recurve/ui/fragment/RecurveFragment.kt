ff32a58c55d19dec1eae79d1f608b68ffc40eb93
TPJ <514820184@qq.com>
2019-01-16 14:25:20 +0800

    update loadingStrategy

--- recurve/src/main/java/com/tangpj/recurve/ui/fragment/RecurveFragment.kt
+++ recurve/src/main/java/com/tangpj/recurve/ui/fragment/RecurveFragment.kt
@@ -1,12 +1,34 @@
 package com.tangpj.recurve.ui.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import com.tangpj.recurve.ui.strategy.LoadingStrategy
 
-class RecurveFragment : Fragment(){
+abstract class RecurveFragment : Fragment(){
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var mLoadingStrategy: LoadingStrategy? = null
+
+    abstract fun onCreateBinding(inflater: LayoutInflater,
+                                 container: ViewGroup?,
+                                 savedInstanceState: Bundle?): ViewDataBinding?
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = onCreateBinding(inflater, container, savedInstanceState)
+        binding?.let {
+            mLoadingStrategy = loadingStrategy()
+        }
+        mLoadingStrategy?.let {
+            
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
+
+    fun loadingStrategy(): LoadingStrategy? = null
 
 }
--- recurve/src/main/java/com/tangpj/recurve/ui/strategy/LoadingStrategy.kt
+++ recurve/src/main/java/com/tangpj/recurve/ui/strategy/LoadingStrategy.kt
@@ -1,20 +1,26 @@
 package com.tangpj.recurve.ui.strategy
 
-import android.app.AlertDialog
 import android.app.Dialog
-import android.view.LayoutInflater
 import android.view.View
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 
 
 /**
- * 家在回调策略
+ * 加载回调策略
  * 
- * @className:
+ * @className: LoadingStrategy
  * @author: tangpengjian113
  * @createTime: 2019/1/15 12:24
+ *
+ * @param lifecycleOwner 监听生命周期
+ * @param content 组建的布局内容（加载完后要显示的布局）
  */
-abstract class LoadingStrategy{
+abstract class LoadingStrategy(
+        private val lifecycleOwner: LifecycleOwner,
+        private val content: View){
 
     //重试事件
     var retryCallback: (() -> Unit)? = null
@@ -22,41 +28,48 @@ abstract class LoadingStrategy{
     //取消事件
     var cancelCallback: (() -> Boolean)? = null
 
-    /**
-     * 加载回调事件
-     *
-     * @method: loadingCallback
-     * @author: tangpengjian113
-     * @createTime: 2019/1/15 13:06
-     *
-     * @return isLoading 是否加载中
-     *         content 加载成功后现实的内容
-     */
-    abstract fun loadingCallback() : (isLoading: Boolean, content: View) -> Unit
+    //Loading回调LiveData
+    open var loadingLiveData: LiveData<Boolean>? = null
 
 }
 
-abstract class ViewLoadingStrategy(private val binding: ViewDataBinding) : LoadingStrategy() {
+abstract class ViewLoadingStrategy(
+        val lifecycleOwner: LifecycleOwner,
+        val content: View,
+        private val loadingViewBinding: ViewDataBinding
+        ) : LoadingStrategy(lifecycleOwner,content) {
 
-    override fun loadingCallback(): (isLoading: Boolean, content: View) -> Unit = {isLoading, content ->
-        if (isLoading){
-            content.visibility = View.GONE
-            binding.root.visibility = View.VISIBLE
-        }else{
-            content.visibility = View.VISIBLE
-            binding.root.visibility = View.GONE
+    override var loadingLiveData: LiveData<Boolean>?
+        get() = super.loadingLiveData
+        set(value) {
+            value?.observe(lifecycleOwner, Observer {
+                if(it){
+                    content.visibility = View.GONE
+                    loadingViewBinding.root.visibility = View.VISIBLE
+                }else{
+                    content.visibility = View.VISIBLE
+                    loadingViewBinding.root.visibility = View.GONE
+                }
+            })
         }
-    }
-
 }
 
- class DialogLoadingStrategy(private val mDialog: Dialog ) : LoadingStrategy() {
+class DialogLoadingStrategy(
+        val lifecycleOwner: LifecycleOwner,
+        val content: View,
+        private val mDialog: Dialog)
+     : LoadingStrategy(lifecycleOwner,content) {
 
-     override fun loadingCallback(): (isLoading: Boolean, content: View) -> Unit =  { isLoading, _ ->
-         if(isLoading) {
-             mDialog.show()
-         } else {
-             mDialog.dismiss()
-         }
-     }
- }
+    override var loadingLiveData: LiveData<Boolean>?
+        get() = super.loadingLiveData
+        set(value) {
+            value?.observe(lifecycleOwner, Observer {
+                if (it){
+                    mDialog.show()
+                }else{
+                    mDialog.dismiss()
+                }
+            })
+        }
+
+}
