0eff0c0d8bdaa8ee9a74840651189493d4ea6a21
TPJ <514820184@qq.com>
2019-01-17 21:25:06 +0800

    add ListViewCreator

---
+++ recurve/src/main/java/com/tangpj/recurve/ui/creator/ListViewCreator.kt
@@ -0,0 +1,33 @@
+package com.tangpj.recurve.ui.creator
+
+import com.tangpj.recurve.widget.Creator
+
+
+/**
+ * 列表布局创建器
+ *
+ * @className: ListViewCreator
+ * @author: tpj
+ * @createTime: 2019/1/17 20:46
+ */
+interface ListViewCreator{
+
+    /**
+     * 添加内容创建器
+     *
+     * @method: addItemCreator
+     * @author: tangpengjian113
+     * @createTime: 2019/1/17 21:07
+     * @param creator Item内容创建器
+     */
+    fun addItemCreator(creator: Creator)
+
+    /**
+     * 添加内容创建器到指定位置
+     *
+     * @method: addItemCreator
+     * @author: tangpengjian113
+     * @createTime: 2019/1/17 21:08
+     */
+    fun addItemCreator(index: Int, creator: Creator)
+}
