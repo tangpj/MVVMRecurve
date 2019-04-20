6f06168b92c382a53df966e3470042d22273246c
TPJ <514820184@qq.com>
2019-04-20 22:46:15 +0800

    update module paging demo

--- app/src/main/java/com/tangpj/mvvmrecurve/paging/BookCreator.kt
+++ app/src/main/java/com/tangpj/mvvmrecurve/paging/BookCreator.kt
@@ -6,12 +6,16 @@ import com.tangpj.adapter.adapter.ModulesAdapter
 import com.tangpj.adapter.creator.ItemCreator
 import com.tangpj.adapter.creator.RecurveViewHolder
 import com.tangpj.mvvmrecurve.databinding.BookItemBinding
+import timber.log.Timber
 
 class BookCreator(adapter: ModulesAdapter) : ItemCreator<Book, BookItemBinding>(adapter,2){
+
     override fun onBindItemView(itemHolder: RecurveViewHolder<BookItemBinding>?, e: Book?, inCreatorPosition: Int) {
+        e ?: return
         itemHolder?.let {
             it.binding.book = e
         }
+        Timber.d(e.toString())
     }
 
     override fun onCreateItemViewHolder(parent: ViewGroup, viewType: Int): RecurveViewHolder<*> {
