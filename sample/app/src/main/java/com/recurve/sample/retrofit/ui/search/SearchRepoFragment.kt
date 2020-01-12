47a70f77482ef2aad0b7438ea7230752571d2e09
TPJ <514820184@qq.com>
2020-01-12 23:08:34 +0800

    update sample

--- sample/app/src/main/java/com/recurve/sample/retrofit/ui/search/SearchRepoFragment.kt
+++ sample/app/src/main/java/com/recurve/sample/retrofit/ui/search/SearchRepoFragment.kt
@@ -1,2 +1,24 @@
 package com.recurve.sample.retrofit.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.recurve.core.ui.fragment.RecurveFragment
+import com.recurve.sample.R
+import com.recurve.sample.databinding.FragmentSearchRepoBinding
+
+class SearchRepoFragment : RecurveFragment(){
+
+    private lateinit var binding: FragmentSearchRepoBinding
+
+    override fun onCreateBinding(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): ViewDataBinding {
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_search_repo, container, false)
+        return binding
+    }
+
+
+
+}
